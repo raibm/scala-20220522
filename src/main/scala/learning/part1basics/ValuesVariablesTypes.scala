fb8e60380fe9392b44b1949ae7aab927f34856f8
Rai Bizerra Maciel <45213677+raibm@users.noreply.github.com>
2022-05-22 16:26:32 -0300

    Variables, values and types (VAL and VARS)

---
+++ src/main/scala/learning/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,20 @@
+package learning.part1basics
+
+object ValuesVariablesTypes extends App {
+
+  // VALS ARE IMMUTABLE :O
+  // PREFER VALS OVER VARS
+  val x: Int = 42
+  println(x)
+
+  val aString: String = "hi"
+  val aChar: Char = 'a'
+  val anInt: Int = 44
+  val aShort: Short = 32767 //max value
+  val aDouble: Double = 6.4
+  val aBoolean: Boolean = true
+
+  // VAR ARE MUTABLE
+  var y: Int = 44
+
+}
